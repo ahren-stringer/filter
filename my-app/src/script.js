7aa5062f1b9545617b73b36bac92aeaf173d1078
ahren-stringer <pavel12g@mail.ru>
2020-11-22 04:25:58 +0300

    ну наконец то

--- my-app/src/script.js
+++ my-app/src/script.js
@@ -4,34 +4,41 @@ logo = document.getElementById("logo").style;
 
 audio = document.getElementById("audio");
 
-window.onclick = function(){
-    if(!context){
+window.onclick = function () {
+    if (!context) {
         preparation();
     }
-    if(audio.paused){
+    if (audio.paused) {
         audio.play();
         loop();
-    }else{
+    } else {
         audio.pause();
     }
 }
 
-function preparation(){
+function preparation() {
     context = new AudioContext();
     analyser = context.createAnalyser();
     src = context.createMediaElementSource(audio);
-    src.connect(analyser);
+
+    var filter = context.createBiquadFilter();
+    filter.type = 'lowpass'; // тип фильтра
+    filter.frequency.value = 100; // частота
+    filter.Q.value = 0.7;
+
+    src.connect(filter);
+    filter.connect(analyser)
     analyser.connect(context.destination);
     loop();
 }
 
-function loop(){
-    if(!audio.paused){
+function loop() {
+    if (!audio.paused) {
         window.requestAnimationFrame(loop);
     }
     array = new Uint8Array(analyser.frequencyBinCount);
     analyser.getByteFrequencyData(array);
 
-    logo.minHeight = (array[40])+"px";
-    logo.width =  (array[40])+"px";
+    logo.minHeight = (array[40]) + "px";
+    logo.width = (array[40]) + "px";
 }
