afc58da65570158e1e3ea0f4618ae9e718ed3198
ahren-stringer <pavel12g@mail.ru>
2020-12-12 20:48:03 +0300

    almost over

--- my-app/src/script.js
+++ my-app/src/script.js
@@ -1,44 +1,57 @@
-var audio, context, analyser, src, array, logo;
-let map=new Map();
+//var audio, context, analyser, src, array, logo;
 
-document.getElementById('player').onclick = function (event) {
-    if (event.target.id=='filter'){
-        audio = event.target.parentElement.querySelector('audio')
-        // if (!context) {
-            preparation(+audio.class);
-        // }
-        event.target.hidden=true
-        if (audio.paused) {
-            audio.play();
-            loop();
-        } else {
-            audio.pause();
-        }
-    }
-}
+// document.getElementById('player').onclick = function (event) {
+//     let hightFr = event.target.parentElement.querySelectorAll('input')[0].value;
+//     let lowFr = event.target.parentElement.querySelectorAll('input')[1].value;
+//     var audioNew = document.createElement('audio');
+//     //audio.class = String(fr);
+//     audioNew.src = audioUrl;
+//     audioNew.controls = true;
 
-function preparation(fr) {
-    context = new AudioContext();
-    src = context.createMediaElementSource(audio);
+//     var message = document.createElement("div")
+//     message.classList.add('filtered__message')
+//     divFilter.append(audio)
+//     if (event.target.id == 'filter__polos') {
+//         audio = event.target.parentElement.querySelectorAll('audio')[event.target.parentElement.querySelectorAll('audio').length - 1]
+//         preparation(+document.getElementById('lowFr').value, +document.getElementById('hightFr').value);
+//         message.innerHTML = 'Звуковое сообщение на выходе фильтра c полосой пропускания от ' + lowFr + ' Гц' + ' до ' + hightFr + ' Гц'
+//         if (audio.paused) {
+//             audio.play();
+//             loop();
+//         } else {
+//             audio.pause();
+//         }
+//     }
+//     if (event.target.id == 'filter__rej') {
+//         audio = event.target.parentElement.querySelectorAll('audio')[event.target.parentElement.querySelectorAll('audio').length - 1]
+//         preparation(+document.getElementById('hightFr').value, +document.getElementById('lowFr').value);
+//         message.innerHTML = 'Звуковое сообщение на выходе фильтра c полосой подавления от ' + lowFr + ' Гц' + ' до ' + hightFr + ' Гц'
+//         if (audio.paused) {
+//             audio.play();
+//             loop();
+//         } else {
+//             audio.pause();
+//         }
+//     }
+//     divFilter.append(messageNew)
+// }
 
-    var filter = context.createBiquadFilter(fr);
-    filter.type = 'lowpass'; // тип фильтра
-    filter.frequency.value = fr; // частота
-    filter.Q.value = 0.7;
-debugger
-    src.connect(filter);
-    filter.connect(context.destination);
-    }
-    loop();
-//}
+// function preparation(lowFr, hightFr) {
+//     context = new AudioContext();
+//     src = context.createMediaElementSource(audio);
+//     //let fr=(lowFr+hightFr)/2;
 
-function loop() {
-    // if (!audio.paused) {
-    //     window.requestAnimationFrame(loop);
-    // }
-    // array = new Uint8Array(analyser.frequencyBinCount);
-    // analyser.getByteFrequencyData(array);
+//     var FNCH = context.createBiquadFilter(hightFr);
+//     FNCH.type = 'lowpass'; // тип фильтра
+//     FNCH.frequency.value = hightFr; // частота
+//     FNCH.Q.value = 0.7;
 
-    // logo.minHeight = (array[40]) + "px";
-    // logo.width = (array[40]) + "px";
-}
+//     var FVCH = context.createBiquadFilter(lowFr);
+//     FVCH.type = 'highpass'; // тип фильтра
+//     FVCH.frequency.value = lowFr; // частота
+//     FVCH.Q.value = 0.7;
+
+//     src.connect(FVCH);
+//     FVCH.connect(FNCH);
+//     FNCH.connect(context.destination)
+// }
